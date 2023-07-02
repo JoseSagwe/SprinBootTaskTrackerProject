1dbb3ca0274090f2115a3bb9254d7b60b4cc07f7
JoseSagwe <Josephsagwe421@gmail.com>
2023-07-02 22:03:57 +0300

    first commit

---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/welcomepage.jsp
@@ -0,0 +1,12 @@
+<%@include file="common/header.jspf"%>
+<%@include file="common/navigation.jspf"%>
+
+<div class="container">
+    <h1>Welcome To JS Software Station</h1>
+    <hr>
+      Your username is: ${username}
+<hr>
+<div  class="container"><a href="list-todos">Manage</a>Your Todos</div>
+</div>
+
+<%@include file="common/footer.jspf"%>
---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001,'Developer Joseph', 'Get AWS Certified', CURRENT_DATE(), false);
+
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002,'Developer Joseph', 'Get Azure Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003,'Developer Joseph', 'Get GCP Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004,'Developer Joseph', 'Learn DevOps', CURRENT_DATE(), false);
